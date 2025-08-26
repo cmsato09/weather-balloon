f05fbebcf21aab9969b536e790c24ad1655583fc
Chris Sato <cmsato09.git@gmail.com>
2025-08-25 17:15:35 -0700

    Show Globe on single page

---
+++ src/components/globe.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Globe from 'react-globe.gl';
+
+const MappingGlobe = () => {
+  return (
+    <Globe
+      globeImageUrl="//unpkg.com/three-globe/example/img/earth-dark.jpg"
+    />
+  );
+};
+
+export default MappingGlobe;
