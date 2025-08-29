3a7ca308bc7b1a5d9ff7cb0c722ef71488843cbb
Chris Sato <cmsato09.git@gmail.com>
2025-08-29 15:54:45 -0700

    Created a new barebones page to see if router works

---
+++ src/pages/MapPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function MapPage() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Map Page</h1>
+      </header>
+      <main>
+        <p>This is the map page.</p>
+      </main>
+    </div>
+  );
+}
+
+export default MapPage;
