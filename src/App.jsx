ea5a60cc9b8383d21c5c8b3cb0e0ce85db4b4988
Chris Sato <cmsato09.git@gmail.com>
2025-08-29 15:56:29 -0700

    Include Navbar in App and BrowserRouter in main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import MappingGlobe from './components/globe.jsx';
-import { fetchAPIData } from './services/api.js';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import MapPage from './pages/MapPage';
+import Navbar from './components/Navbar';
 import './App.css';
 
 function App() {
-  const [balloonData, setBalloonData] = useState([]);
-
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        const data = await fetchAPIData();
-        setBalloonData(data);
-      } catch (error) {
-        console.error("Error fetching balloon data:", error);
-      }
-    };
-
-    getData();
-  }, []);
-
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>24-Hour Path Visualization</h1>
-      </header>
-      <main>
-        <MappingGlobe balloonData={balloonData} />
-      </main>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/map" element={<MapPage />} />
+      </Routes>
     </div>
   );
 }
