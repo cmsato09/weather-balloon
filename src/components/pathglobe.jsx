68108feeed61ab8d543bb0348dffc08ebc7cfcee
Chris Sato <cmsato09.git@gmail.com>
2025-09-04 16:13:22 -0700

    Add labels and current coord of balloons

--- src/components/pathglobe.jsx
+++ src/components/pathglobe.jsx
@@ -6,8 +6,18 @@ const PathGlobe = ({ balloonsData }) => {
     return <div>Loading globe...</div>;
   }
 
-
   const colors = ['#ff0000', '#00ff00', '#0000ff', '#ffff00', '#ff00ff', '#00ffff'];
+
+  const labelData = balloonsData.map(path => {
+    const currentPos = path.coords[0];
+    return {
+      lat: currentPos[0],
+      lng: currentPos[1],
+      alt: currentPos[2] / 1000,
+      text: `#${path.balloon_index}`,
+      color: colors[path.balloon_index % colors.length]
+    };
+  });
 
   return (
     <Globe
@@ -18,8 +28,17 @@ const PathGlobe = ({ balloonsData }) => {
       pathPointLng={p => p[1]}
       pathPointAltitude={p => p[2] / 1000}
       pathColor={(path) => colors[path.balloon_index % colors.length]}
-      pathLabel={path => path.balloon_index}
-      pathStroke={1}
+      pathLabel={path => `Balloon ${path.balloon_index}`}
+      pathStroke={2}
+      labelsData={labelData}
+      labelLat={d => d.lat}
+      labelLng={d => d.lng}
+      labelAltitude={d => d.alt}
+      labelText={d => d.text}
+      labelSize={0.5}
+      labelDotRadius={0.2}
+      labelColor={d => d.color}
+      labelResolution={2}
     />
   );
 };
