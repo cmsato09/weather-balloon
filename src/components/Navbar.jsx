b2df72237cf3818a978258d846b80aea8b2ded8c
Chris Sato <cmsato09.git@gmail.com>
2025-09-05 17:12:36 -0700

    Navbar ribbon CSS styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 function Navbar() {
   return (
-    <nav>
-      <ul>
+    <nav className="navbar-ribbon">
+      <ul className="navbar-links">
         <li>
           <Link to="/">Home</Link>
         </li>
